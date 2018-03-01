b8a6227b8e8de7863a68110e025902b2c40a5df5
Patrick Nawracay <pnawracay@suse.com>
2018-03-01 12:52:42 +0100

    mgr/dashboard_v2: RGW proxy: Works
    Signed-off-by: Patrick Nawracay <pnawracay@suse.com>

--- src/pybind/mgr/dashboard_v2/controllers/rgw.py
+++ src/pybind/mgr/dashboard_v2/controllers/rgw.py
@@ -2,15 +2,19 @@
 from __future__ import absolute_import
 
 import json
+import re
 
-from .. import logger
+import cherrypy
+
+from .. import logger, mgr
 from ..services.ceph_service import CephService
-from ..tools import ApiController, RESTController, AuthRequired
+from ..tools import ApiController, RESTController, AuthRequired, isset
+from ..components.rgw_client import RgwClient
 
 
 @ApiController('rgw')
 @AuthRequired()
-class Rgw(RESTController):
+class RgwRoot(RESTController):
     pass
 
 
@@ -67,4 +71,44 @@ class RgwDaemon(RESTController):
 
         daemon['rgw_metadata'] = metadata
         daemon['rgw_status'] = status
+
         return daemon
+
+
+@ApiController('rgw/proxy')
+@AuthRequired()
+class RgwProxy(RESTController):
+    @cherrypy.expose
+    def default(self, *vpath, **params):
+        rgw_client = RgwClient.admin_instance()
+        method = cherrypy.request.method
+        path = '/'.join(vpath)
+        data = None
+        if cherrypy.request.body.length is not None:
+            cherrypy.request.body.read()
+
+        return rgw_client.proxy(method, path, params, data)
+
+    @staticmethod
+    def _determine_rgw_addr():
+        service_map = mgr.get('service_map')
+
+        if not isset(service_map, ['services', 'rgw', 'daemons', 'rgw']):
+            msg = 'No RGW found.'
+            raise LookupError(msg)
+
+        daemon = service_map['services']['rgw']['daemons']['rgw']
+        if daemon['metadata']['zonegroup_name'] != 'default' or \
+                daemon['metadata']['zone_name'] != 'default':
+            msg = 'Automatically determining RGW daemon failed.'
+            raise LookupError(msg)
+
+        addr = daemon['addr'].split(':')[0]
+        match = re.search(r'port=(\d+)', daemon['metadata']['frontend_config#0'])
+        if match:
+            port = int(match.group(1))
+        else:
+            msg = 'Failed to determine RGW port'
+            raise LookupError(msg)
+
+        return addr, port
--- src/pybind/mgr/dashboard_v2/tools.py
+++ src/pybind/mgr/dashboard_v2/tools.py
@@ -2,6 +2,7 @@
 # pylint: disable=W0212
 from __future__ import absolute_import
 
+import socket
 import collections
 import datetime
 import importlib
@@ -13,6 +14,10 @@ import sys
 import time
 import threading
 from errno import errorcode
+try:
+    from urlparse import ParseResult
+except ImportError:
+    from urllib.parse import ParseResult
 
 import cherrypy
 
@@ -521,3 +526,57 @@ class NotificationQueue(threading.Thread):
         self.notify_listeners(self._queue)
         self._queue.clear()
         logger.debug("notification queue finished")
+
+
+def is_valid_ipv6_address(addr):
+    try:
+        socket.inet_pton(socket.AF_INET6, addr)
+        return True
+    except socket.error:
+        return False
+
+
+def build_url(host, scheme=None, port=None):
+    """
+    Build a valid URL. IPv6 addresses specified in host will be enclosed in brackets
+    automatically.
+
+    >>> build_url('example.com', 'https', 443)
+    'https://example.com:443'
+
+    >>> build_url(host='example.com', port=443)
+    '//example.com:443'
+
+    >>> build_url('fce:9af7:a667:7286:4917:b8d3:34df:8373', port=80, scheme='http')
+    'http://[fce:9af7:a667:7286:4917:b8d3:34df:8373]:80'
+
+    :param scheme: The scheme, e.g. http, https or ftp.
+    :type scheme: str
+    :param host: Consisting of either a registered name (including but not limited to
+                 a hostname) or an IP address.
+    :type host: str
+    :type port: int
+    :rtype: str
+    """
+    netloc = host if not is_valid_ipv6_address(host) else '[{}]'.format(host)
+    if port:
+        netloc += ':{}'.format(port)
+    pr = ParseResult(scheme=scheme, netloc=netloc, path='', params='', query='', fragment='')
+    return pr.geturl()
+
+
+def isset(dct, keys):
+    """
+    Tests wheter the keys exist recursively in `dictionary`.
+
+    :type dct: dict
+    :type keys: list
+    :rtype: bool
+    """
+    if keys:
+        key = keys.pop(0)
+        if key in dct:
+            dct = dct[key]
+            return isset(dct, keys)
+        return False
+    return True

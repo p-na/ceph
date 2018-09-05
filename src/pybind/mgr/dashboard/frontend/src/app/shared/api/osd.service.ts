36e916de11b6d1630aee4b43cba4a9dc16249adc
Patrick Nawracay <pnawracay@suse.com>
2018-09-05 16:45:17 +0200

    Add OSD actions to OSD service
    Fixes: http://tracker.ceph.com/issues/35448
    
    Signed-off-by: Patrick Nawracay <pnawracay@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/api/osd.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/api/osd.service.ts
@@ -30,4 +30,47 @@ export class OsdService {
   updateFlags(flags: string[]) {
     return this.http.put(`${this.path}/flags`, { flags: flags });
   }
+
+  markOut(id: number) {
+    return this.http.post(`${this.path}/${id}/mark_out`, null);
+  }
+
+  markIn(id: number) {
+    return this.http.post(`${this.path}/${id}/mark_in`, null);
+  }
+
+  markDown(id: number) {
+    return this.http.post(`${this.path}/${id}/mark_down`, null);
+  }
+
+  reweight(id: number, weight: number) {
+    return this.http.post(`${this.path}/${id}/reweight`, {weight: weight});
+  }
+
+  mark_lost(id: number) {
+    return this.http.post(`${this.path}/${id}/mark_lost`, null);
+  }
+
+  create(uuid?: string, id?: number) {
+    let body = null;
+    if (uuid) {
+      body = { uuid: uuid };
+      if (id) {
+        body.svc_id = id;
+      }
+    }
+    return this.http.post(`${this.path}`, body);
+  }
+
+  remove(id: number) {
+    return this.http.post(`${this.path}/${id}/remove`, null);
+  }
+
+  destroy(id: number) {
+    return this.http.post(`${this.path}/${id}/destroy`, null);
+  }
+
+  safeToDestroy(id: number) {
+    return this.http.get(`${this.path}/${id}/safe_to_destroy`);
+  }
 }

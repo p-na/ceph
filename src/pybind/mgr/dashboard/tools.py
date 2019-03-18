db434702d597c16c1128c35bd3d985a7c9ba03b5
Patrick Nawracay <pnawracay@suse.com>
2019-03-18 12:16:16 +0000

    mgr/dashboard: tools.py: Update documentation and add doctest

--- src/pybind/mgr/dashboard/tools.py
+++ src/pybind/mgr/dashboard/tools.py
@@ -919,13 +919,26 @@ def find_object_in_list(key, value, iterable):
 
 def update_dict(data, update_data):
     # type: (Dict[Any, Any], Dict[Any, Any]) -> Dict[Any]
-    """
-    Update a dictionary recursively.
+    """ Update a dictionary recursively.
 
-    Eases doing so by providing the option to separate the key to be updated with dot characters.
+    Eases doing so by providing the option to separate the key to be updated by dot characters.  If
+    a key provided does not exist, it will raise an KeyError instead of just updating the
+    dictionary.
+
+    Limitations
+
+    Please note that the functionality provided by this method can only be used if the dictionary to
+    be updated (`data`) does not contain dot characters in its keys.
+
+    :raises KeyError:
 
     >>> update_dict({'foo': {'bar': 5}}, {'foo.bar': 10})
     {'foo': {'bar': 10}}
+
+    >>> update_dict({'foo': {'bar': 5}}, {'xyz': 10})
+    Traceback (most recent call last):
+        ...
+    KeyError: 'xyz'
     """
     for k, v in update_data.items():
         keys = k.split('.')
